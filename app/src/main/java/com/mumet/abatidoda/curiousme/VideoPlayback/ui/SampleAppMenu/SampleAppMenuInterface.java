c1344d6aabd2b384cfc94a86474a1a8fcf1b0e1c
Vrishank <vrishank98@gmail.com>
2019-01-26 16:56:18 +0530

    Refractor
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- app/src/main/java/com/mumet/abatidoda/curiousme/VideoPlayback/ui/SampleAppMenu/SampleAppMenuInterface.java
+++
@@ -1,9 +0,0 @@
-
-package com.mumet.abatidoda.curiousme.VideoPlayback.ui.SampleAppMenu;
-
-// To be implemented in the activities to handle any desired function 
-// previously set for a SampleAppMenu instance
-public interface SampleAppMenuInterface
-{
-    public boolean menuProcess(int command);
-}
